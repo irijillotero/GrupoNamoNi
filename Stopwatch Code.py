f86efa3185fa0bc64e83086b909c37587069cdf3
irijillotero <105407972+irijillotero@users.noreply.github.com>
2022-05-21 10:41:50 +0800

    Add files via upload

---
+++ Stopwatch Code.py
@@ -0,0 +1,15 @@
+from tkinter  import *
+import time
+
+root=Tk()
+root.configure(background=("black"))
+root.title("Stopwatch")
+root.geometry("980x480")
+
+time_elapsed1=0
+time_elapsed2=0
+time_elapsed3=0
+time1=0
+time2=0
+i=0
+j=0
